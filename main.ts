54a9df809a611c1d5e62dbd1d4a23fc31f68d9e7
tlauouvea <61261560+tlauouvea@users.noreply.github.com>
2020-07-16 11:02:02 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,46 @@
 input.onButtonPressed(Button.A, function () {
-    led.unplot(posX, 0)
-    posX += -1
-    if (posX < 0) {
-        posX = 0
+    if (posX == 0) {
+        if (posY == 0) {
+            led.unplot(posX, posY)
+            led.plot(4, 4)
+            posX = 4
+            posY = 4
+        } else {
+            led.unplot(posX, posY)
+            led.plot(4, posY - 1)
+            posX = 4
+            posY = posY - 1
+        }
+    } else {
+        led.unplot(posX, posY)
+        led.plot(posX - 1, posY)
+        posX = posX - 1
     }
 })
 input.onButtonPressed(Button.B, function () {
-    led.unplot(posX, 0)
-    posX += 1
-    if (posX > 4) {
-        posX = 4
+    if (posX == 4) {
+        if (posY == 4) {
+            led.unplot(posX, posY)
+            led.plot(0, 0)
+            posX = 0
+            posY = 0
+        } else {
+            led.unplot(posX, posY)
+            led.plot(0, posY + 1)
+            posX = 0
+            posY = posY + 1
+        }
+    } else {
+        led.unplot(posX, posY)
+        led.plot(posX + 1, posY)
+        posX = posX + 1
     }
 })
 let posX = 0
+let posY = 0
+posY = 0
 posX = 0
+led.plot(0, 0)
 basic.forever(function () {
-    led.plot(Math.constrain(posX, 0, 4), 0)
+	
 })
